fac9a97dfbcf43cfbeca3f1bf6d5e65ee1e5e688
TNMayer <thomas.mayer@smail.auswaertiges-amt.de>
2021-04-23 07:00:21 +0200

    required changes implemented

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -66,9 +66,9 @@ module.exports = {
             filename: "./index.html"
         }),
         new CleanWebpackPlugin(),
-        new WorkboxPlugin.GenerateSW()
+        // new WorkboxPlugin.GenerateSW()
     ],
     performance: {
         hints: false,
-    }  
+    }
 }
